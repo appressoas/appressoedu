7b55ab7938eba3824d6c5300109fc8059692d171
Magne Westlie <magne@appresso.no>
2015-02-03 10:46:50 +0100

    Added some code for motivating using classes and object oriented programming.

---
+++ python/class_motivation.py
@@ -0,0 +1,31 @@
+"""
+We want to store name, email and study for many students.
+
+This is how we could do it, but it would never be a useful program.
+"""
+
+student1_name = "Jeffrey"
+student1_email = "jeffrey@example.com"
+student1_study = "Computer Science"
+
+student2_name = "Walter"
+student2_email = "walter@example.com"
+student2_study = "Mathematics"
+
+student3_name = "Donny"
+student3_email = "donny@example.com"
+student3_study = "Anthropology"
+
+student4_name = "Jesus"
+student4_email = "jesus@example.com"
+student4_study = ""
+
+print student1_name
+print student1_email
+print student1_study
+
+print student2_name
+print student2_email
+print student2_study
+
+# and so on.
