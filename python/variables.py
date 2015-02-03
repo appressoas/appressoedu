368d885af7782410efbc01a6bda53d4db0b9c34e
Tor Ivar Johansen <tor@torjohansen.no>
2015-02-03 08:50:03 +0100

    Added variables module with comments

---
+++ python/variables.py
@@ -0,0 +1,8 @@
+# age er blitt et heltall
+age = 14
+# number er blitt et desimaltall
+number = 13.5
+# text er blitt en tekststreng
+text = "This is a text string"
+# text er blitt heltall
+text = 14
