45b979898ac389a7380e9067e65b2db283902992
Magne Westlie <magne@appresso.no>
2015-02-03 10:23:29 +0100

    Added motivation for using functions.

---
+++ python/function_example_motivation.py
@@ -0,0 +1,24 @@
+# Example using a function that we created in the same file.
+
+person1_age = 17
+person2_age = 18
+person3_age = 20
+
+required_age_for_lisence = 18
+
+if person1_age >= required_age_for_lisence:
+    print "Person is", person1_age, "and can take the test."
+else:
+    print "Person cannot take the test yet! Age is", person1_age
+
+if person2_age >= required_age_for_lisence:
+    print "Person is", person2_age, "and can take the test."
+else:
+    print "Person cannot take the test yet! Age is", person2_age
+
+if person3_age >= required_age_for_lisence:
+    print "Person is", person3_age, "and can take the test."
+else:
+    print "Person cannot take the test yet! Age is", person3_age
+
+
