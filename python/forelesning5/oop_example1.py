c4725805a02c954ecc80306c3aa55db67ac085b7
Magne Westlie <magne@appresso.no>
2015-02-10 09:33:48 +0100

    Added .DS_Store mac file to gitignore. Added an example of not using oop when working with objects.

---
+++ python/forelesning5/oop_example1.py
@@ -0,0 +1,37 @@
+"""
+Make a program that stores information on students.
+We want name, email and study.
+"""
+student1_name = "Per"
+student1_email = "per@example.com"
+student1_study = "Computer Science"
+
+student2_name = "Kari"
+student2_email = "kari@example.com"
+student2_study = "Mathematics"
+
+student3_name = "Jens"
+student3_email = "jens@example.com"
+student3_study = "Mathematics"
+
+print "Navn:", student1_name
+print "Email:", student1_email
+print
+print "Navn:", student2_name
+print "Email:", student2_email 
+print 
+print "Navn:", student3_name
+print "Email:", student3_email 
+
+
+
+
+
+
+
+
+
+
+
+
+
